d07564671d8a5482d46d72eeb4f5dc5730e5c687
Ulrik-Matemu <advancechronicles@gmail.com>
2025-02-26 10:29:34 +0300

    First Commit, Prepared dev environment

---
+++ vite.config.ts
@@ -0,0 +1,69 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      srcDir: "public",
+      filename: "sw.js",
+      strategies: "injectManifest",
+      devOptions: {
+        enabled: true, // Enable PWA in development mode
+      },
+      manifest: {
+        name: "Sosika",
+        short_name: "Sosika",
+        description: "An Aggregator for food delivery services in campuses",
+        theme_color: "#ffffff",
+        background_color: "#f8f9fa",
+        display: "standalone",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          }
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === "document",
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "html-cache",
+            },
+          },
+          {
+            urlPattern: ({ request }) =>
+              ["style", "script", "worker"].includes(request.destination),
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "assets-cache",
+            },
+          },
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
