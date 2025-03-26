05c98726cbc845a68cb434c542ea92997967f262
Ulrik-Matemu <advancechronicles@gmail.com>
2025-03-26 21:18:56 +0300

    deploying on netlify #4

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,28 +1,3 @@
-import { precacheAndRoute } from "workbox-precaching";
-
-precacheAndRoute(self.__WB_MANIFEST);
-
-self.addEventListener("install", (event) => {
-    console.log("Service Worker installing...");
-    event.waitUntil(
-      caches.open("app-cache").then((cache) => {
-        return cache.addAll(["/", "/index.html"]);
-      })
-    );
-  });
-  
-  self.addEventListener("activate", (event) => {
-    console.log("Service Worker activated.");
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-
 
 // Do NOT use import statements in service workers!
 // Use importScripts instead
