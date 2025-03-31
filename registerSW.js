af42c1050a66faed0faba2c47ffce91510d54d9a
Ulrik-Matemu <advancechronicles@gmail.com>
2025-03-31 15:46:49 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/app.sosika/sw.js', { scope: '/app.sosika/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
