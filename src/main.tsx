7e16795d8efc0f1684294a8863293e1a1369ffaf
Ulrik-Matemu <advancechronicles@gmail.com>
2025-03-31 19:41:18 +0300

    retrying firebase in server #13

--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,7 @@ import App from './App.tsx'
 
 applyStoredTheme();
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register('/firebase-messaging-sw.js')
-    .then((registration) => {
-      console.log("Service Worker registered:", registration);
-    })
-    .catch((error) => {
-      console.error("Service Worker registration failed:", error);
-    });
-}
+
 
 
 createRoot(document.getElementById('root')!).render(
