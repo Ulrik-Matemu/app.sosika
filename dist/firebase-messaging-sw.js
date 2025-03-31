0472a85e7b90aa04df1064ff60da53f1f369ceb3
Ulrik-Matemu <advancechronicles@gmail.com>
2025-03-31 11:41:08 +0300

    retrying firebase in server #4

--- dist/firebase-messaging-sw.js
+++ dist/firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-const { type } = require("os");
+
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
