dd8aa6059daa6623d4077bb8ecaf3e45dfaa8e98
Ulrik-Matemu <advancechronicles@gmail.com>
2025-03-31 11:41:54 +0300

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -34,15 +34,6 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-const CACHE_NAME = "sosika-cache-v1";
-const ASSETS_TO_CACHE = ["/", "/index.html"];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => 
-      cache.addAll(ASSETS_TO_CACHE))
-  );
-});
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
