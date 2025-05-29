ae8aa22ff178566d6b0aec10430926c02bff65a4
Ulrik-Matemu <advancechronicles@gmail.com>
2025-05-29 12:39:09 +0300

    setting maintenance mode

---
+++ src/pages/maintenance.tsx
@@ -0,0 +1,19 @@
+export const Maintenance = () => {
+    return (
+        
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4">
+            <img
+            src="/maintenance.svg"
+            alt="Maintenance"
+            className="w-40 h-40 sm:w-64 sm:h-64 mb-4"
+            style={{ maxWidth: '100%', height: 'auto' }}
+            />
+            <h1 className="text-2xl sm:text-4xl font-bold text-gray-800 mb-4 text-center">
+            Maintenance Mode
+            </h1>
+            <p className="text-base sm:text-lg text-gray-600 mb-8 text-center">
+            We are currently undergoing maintenance. Please check back later.
+            </p>
+        </div>
+    );
+}
