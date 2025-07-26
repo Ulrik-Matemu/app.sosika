289ad80458814c6e8a43d68f6ba250c2d368ee58
Ulrik-Matemu <advancechronicles@gmail.com>
2025-07-26 14:07:47 +0300

    changed hashrouter to browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import { RegisterPage } from "./pages/register";
 // import WelcomePage from "./pages/welcome";
