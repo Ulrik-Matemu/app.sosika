d7c2a82ad99d8c99e776c7c08aa047603fb6b8cf
Ulrik-Matemu <advancechronicles@gmail.com>
2025-08-25 18:48:30 +0300

    fixed auth issue limiting access to pages that dont need auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import { useEffect } from "react";
 import { RegisterPage } from "./pages/register";
-// import WelcomePage from "./pages/welcome";
 import LoginPage from "./pages/login";
 import MenuExplorer from "./pages/explore";
 import OrderTrackingWithErrorBoundary from "./components/my-components/orderTracking";
@@ -9,17 +8,16 @@ import OrdersPage from "./pages/orders";
 import ProfileManagement from "./pages/profile";
 import ForgotPassword from "./pages/forgot-password";
 import ResetPassword from "./pages/reset-password";
-// import { Maintenance } from "./pages/maintenance";
 import "./App.css";
-import { TooltipProvider } from "./components/ui/tooltip"; // Ensure correct import
-import { listenForForegroundMessages } from './push-notifications'
+import { TooltipProvider } from "./components/ui/tooltip";
+import { listenForForegroundMessages } from "./push-notifications";
 import { setupPushNotifications } from "./services/push-notifications";
 import { analytics, logEvent } from "./firebase";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/cartContext";
 import { AuthRedirect } from "./pages/AuthRedirects";
 import VendorPage from "./pages/vendor";
-import  PageWrapper  from "./services/page-transition";
+import PageWrapper from "./services/page-transition";
 import Waitlist from "./pages/waitlist";
 import Browser from "./pages/browse";
 import VendorRegistration from "./pages/vendorRegistration";
@@ -27,7 +25,6 @@ import VendorProfile from "./pages/vendor/profile";
 import VendorCatalogPage from "./pages/vendor/menuItems";
 import VendorOrders from "./pages/vendor/orders";
 import MenuItemScreen from "./pages/menuItemScreen";
-
 
 function App() {
   useEffect(() => {
@@ -44,46 +41,60 @@ function App() {
 
     window.addEventListener("beforeunload", handleBeforeUnload);
     return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload); 
+      window.removeEventListener("beforeunload", handleBeforeUnload);
     };
   }, []);
-
 
   useEffect(() => {
     setupPushNotifications();
     listenForForegroundMessages();
   }, []);
 
+  /**
+   * PrivateRoutes wrapper
+   * Wraps all protected routes with AuthProvider
+   */
+  const PrivateRoutes = () => {
+    return (
+      <AuthProvider>
+        <Outlet />
+      </AuthProvider>
+    );
+  };
+
   return (
-    <AuthProvider>
-      <CartProvider>
-        <Router>
-          <TooltipProvider>
-            <PageWrapper>
-              <Routes>
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/reset-password" element={<ResetPassword />} />
+    <CartProvider>
+      <Router>
+        <TooltipProvider>
+          <PageWrapper>
+            <Routes>
+              {/* Public Routes */}
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/reset-password" element={<ResetPassword />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/waitlist" element={<Waitlist />} />
+
+              {/* Private Routes */}
+              <Route element={<PrivateRoutes />}>
                 <Route path="/" element={<AuthRedirect />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
                 <Route path="/explore" element={<MenuExplorer />} />
                 <Route path="/order-tracking/:orderId" element={<OrderTrackingWithErrorBoundary />} />
                 <Route path="/orders" element={<OrdersPage />} />
                 <Route path="/profile" element={<ProfileManagement />} />
                 <Route path="/vendor" element={<VendorPage vendorId={29} />} />
-                <Route path="/waitlist" element={<Waitlist />} />
-                <Route path="/browse" element={<Browser />} />
                 <Route path="/vendor-registration" element={<VendorRegistration />} />
                 <Route path="/vendor-profile" element={<VendorProfile />} />
                 <Route path="/vendor-catalog" element={<VendorCatalogPage />} />
                 <Route path="/vendor-orders" element={<VendorOrders />} />
                 <Route path="/menu-item/:id" element={<MenuItemScreen />} />
-              </Routes>
-            </PageWrapper>
-          </TooltipProvider>
-        </Router>
-      </CartProvider>
-    </AuthProvider>
+                <Route path="/browse" element={<Browser />} />
+              </Route>
+            </Routes>
+          </PageWrapper>
+        </TooltipProvider>
+      </Router>
+    </CartProvider>
   );
 }
 
